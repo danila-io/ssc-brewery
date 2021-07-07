5d5b4aeb2be12323983591d16e61e8f50f98a57f
Danila <4106339-danila-io@users.noreply.gitlab.com>
2021-07-07 21:12:27 +0400

    Allow getting beer via the REST API

--- src/test/java/guru/sfg/brewery/web/controllers/BaseIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BaseIT.java
@@ -19,7 +19,7 @@ public abstract class BaseIT {
     @Autowired
     WebApplicationContext webApplicationContext;
 
-    MockMvc mockMvc;
+    protected MockMvc mockMvc;
 
     @MockBean
     BeerRepository beerRepository;
---
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerIT.java
@@ -0,0 +1,23 @@
+package guru.sfg.brewery.web.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class BeerRestControllerIT extends BaseIT {
+
+    @Test
+    void findBeers() throws Exception {
+        mockMvc.perform(get("/api/v1/beer"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findBeerById() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/6de204c4-57e1-4793-b2ee-48ba84c0ff9a"))
+                .andExpect(status().isOk());
+    }
+}
