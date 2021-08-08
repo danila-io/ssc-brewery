138123d5f6c73ef806cf5657781e3c4bdc886c85
Danila <4106339-danila-io@users.noreply.gitlab.com>
2021-08-08 21:57:35 +0400

    Create demo tests for password encoders, use Delegating encoder

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package guru.sfg.brewery.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -26,19 +29,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().httpBasic();
     }
 
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("spring")
-                .password("{noop}spring")
+                .password("{bcrypt}$2a$10$X6SqHBUQN6fBIZo.UGLKtupihrnufktC3L9BeX5j3HsR2Ix12WX36")
                 .roles("ADMIN")
                 .and()
                 .withUser("user")
-                .password("{noop}password")
+                .password("{sha256}7785a517ed0f8f2f4797bf296ec45797826b414611b150c6b009542937ae03ecd5c63cdae82e15e7")
                 .roles("USER")
                 .and()
                 .withUser("scott")
-                .password("{noop}tiger")
+                .password("{ldap}{SSHA}ynxg//ZNVB9J2a1lfiQxm/A1SVgoAsf3o4kA+A==")
                 .roles("CUSTOMER");
     }
 }
