3791c60dd7b9ee1d2ad535070fb3dfc11f449b2a
Danila <4106339-danila-io@users.noreply.gitlab.com>
2021-09-12 22:25:21 +0400

    Fix the 302 error by providing custom implementations

--- src/main/java/guru/sfg/brewery/security/RestHeaderAuthFilter.java
+++ src/main/java/guru/sfg/brewery/security/RestHeaderAuthFilter.java
@@ -4,10 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.util.StringUtils;
 
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -30,7 +35,11 @@ public class RestHeaderAuthFilter extends AbstractAuthenticationProcessingFilter
 
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
 
-        return this.getAuthenticationManager().authenticate(token);
+        if (!StringUtils.isEmpty(username)) {
+            return this.getAuthenticationManager().authenticate(token);
+        } else {
+            return null;
+        }
     }
 
     private String getUsername(HttpServletRequest request) {
@@ -39,5 +48,38 @@ public class RestHeaderAuthFilter extends AbstractAuthenticationProcessingFilter
 
     private String getPassword(HttpServletRequest request) {
         return request.getHeader("Api-Secret");
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("Request is to process authentication");
+        }
+
+        Authentication authResult = attemptAuthentication(request, response);
+
+        if (authResult != null) {
+            successfulAuthentication(request, response, chain, authResult);
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response, FilterChain chain, Authentication authResult)
+            throws IOException, ServletException {
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("Authentication success. Updating SecurityContextHolder to contain: "
+                    + authResult);
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authResult);
     }
 }
